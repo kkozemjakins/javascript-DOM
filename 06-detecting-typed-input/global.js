57c83fb008b9853b7e2ae6ac8b7db9968ac2d8f0
kkozemjakins <kkozemjakins@gmail.com>
2021-03-16 15:36:49 +0200

    6.uzd

--- 06-detecting-typed-input/global.js
+++ 06-detecting-typed-input/global.js
@@ -1,1 +1,13 @@
 // Your JavaScript goes here.
+window.onload = function(){
+
+    var enterYourName = document.getElementById("full_name");
+  
+    enterYourName.addEventListener("keyup", function(){
+  
+      var greetings = document.getElementById("greeting");
+  
+      greetings.innerHTML = ("Greetings and salutations, " + enterYourName.value + "!");
+    });
+  
+  };
